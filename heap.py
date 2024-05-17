f41c91cff27475ed029c1c92ba57d081e2351c6f
IneseAnisimovaDITF <144724070+IneseAnisimovaDITF@users.noreply.github.com>
2024-05-17 13:49:07 +0000

    17.05.2024

--- heap.py
+++ heap.py
@@ -56,58 +56,84 @@ class MaxHeap:
         return max_value
 
 
+# def find_kth_smallest(nums, k):
+#     max_heap = MaxHeap()
+#     for parmeklevisunums in nums:
+#         max_heap.insert(parmeklevisunums)
+#         if len(max_heap.heap)>k:
+#             max_heap.remove()
+#     return max_heap.heap[0]
+
+# jaievada k elementu un javeic kludu apstrādi ar try except negativs skaitlis teksts skaitla vieta
 def find_kth_smallest(nums, k):
-    ###################################
-    ###                             ###
-    ###                             ###
-    ###                             ###
-    ###################################
-
-
-
-
+    while True:
+        try:
+            if k <= 0:
+                print("kļuda, k ir negatīvs skaitlis vai 0")
+                break
+            skaitamnums = 0
+            for num in nums:
+                skaitamnums += 1
+            if k > skaitamnums:
+                print("kļūda nav tik daudz elementu saraksta nums")
+                break
+            max_heap = MaxHeap()
+            for parmeklevisunums in nums:
+                max_heap.insert(parmeklevisunums)
+                if len(max_heap.heap)>k:
+                    max_heap.remove()
+            return max_heap.heap[0]
+        except:
+            print ("kļūda")
+try: 
+    nums = [3,2,1,5,6,4,2 ,2]
+    k = int(input("k = "))
+    result = find_kth_smallest(nums, k)
+    print(f"{result}")
+except Exception as e:
+    print("kļūda ir ievadits teksts")
 # Test cases
-nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
-ks = [2, 3, 4, 7]
-expected_outputs = [2, 3, 4, 5]
+# nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
+# ks = [2, 3, 4, 7]
+# expected_outputs = [2, 3, 4, 5]
 
-for i in range(len(nums)):
-    print(f'Test case {i+1}...')
-    print(f'Input: {nums[i]} with k = {ks[i]}')
-    result = find_kth_smallest(nums[i], ks[i])
-    print(f'Output: {result}')
-    print(f'Expected output: {expected_outputs[i]}')
-    print(f'Test passed: {result == expected_outputs[i]}')
-    print('---------------------------------------')
+# for i in range(len(nums)):
+#     print(f'Test case {i+1}...')
+#     print(f'Input: {nums[i]} with k = {ks[i]}')
+#     result = find_kth_smallest(nums[i], ks[i])
+#     print(f'Output: {result}')
+#     print(f'Expected output: {expected_outputs[i]}')
+#     print(f'Test passed: {result == expected_outputs[i]}')
+#     print('---------------------------------------')
 
 
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    Test case 1...
-    Input: [3, 2, 1, 5, 6, 4] with k = 2
-    Output: 2
-    Expected output: 2
-    Test passed: True
-    ---------------------------------------
-    Test case 2...
-    Input: [6, 5, 4, 3, 2, 1] with k = 3
-    Output: 3
-    Expected output: 3
-    Test passed: True
-    ---------------------------------------
-    Test case 3...
-    Input: [1, 2, 3, 4, 5, 6] with k = 4
-    Output: 4
-    Expected output: 4
-    Test passed: True
-    ---------------------------------------
-    Test case 4...
-    Input: [3, 2, 3, 1, 2, 4, 5, 5, 6] with k = 7
-    Output: 5
-    Expected output: 5
-    Test passed: True
-    ---------------------------------------
+# """
+#     EXPECTED OUTPUT:
+#     ----------------
+#     Test case 1...
+#     Input: [3, 2, 1, 5, 6, 4] with k = 2
+#     Output: 2
+#     Expected output: 2
+#     Test passed: True
+#     ---------------------------------------
+#     Test case 2...
+#     Input: [6, 5, 4, 3, 2, 1] with k = 3
+#     Output: 3
+#     Expected output: 3
+#     Test passed: True
+#     ---------------------------------------
+#     Test case 3...
+#     Input: [1, 2, 3, 4, 5, 6] with k = 4
+#     Output: 4
+#     Expected output: 4
+#     Test passed: True
+#     ---------------------------------------
+#     Test case 4...
+#     Input: [3, 2, 3, 1, 2, 4, 5, 5, 6] with k = 7
+#     Output: 5
+#     Expected output: 5
+#     Test passed: True
+#     ---------------------------------------
 
-"""
+# """
 
